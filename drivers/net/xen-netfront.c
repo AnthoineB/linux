183191b4ae29de220e6d6823b70a9690a62140a8
Joao Martins <joao.martins@neclab.eu>
2015-05-12 19:18:34 +0200

    xen-netfront: refactor claim/release grant
    Refactors how grants are claimed/released/revoked by moving that code
    into claim_grant and release_grant helpers routines that can be shared
    in both TX/RX path.
    
    Signed-off-by: Joao Martins <joao.martins@neclab.eu>

--- drivers/net/xen-netfront.c
+++ drivers/net/xen-netfront.c
@@ -261,6 +261,37 @@ static void xennet_maybe_wake_tx(struct netfront_queue *queue)
 		netif_tx_wake_queue(netdev_get_tx_queue(dev, queue->id));
 }
 
+static grant_ref_t claim_grant(unsigned long gfn,
+			       grant_ref_t *gref_head,
+			       int otherend_id,
+			       int flags)
+{
+	grant_ref_t ref;
+
+	ref = gnttab_claim_grant_reference(gref_head);
+	WARN_ON_ONCE(IS_ERR_VALUE((unsigned long)(int)ref));
+
+	gnttab_grant_foreign_access_ref(
+		ref, otherend_id, gfn, flags);
+
+	return ref;
+}
+
+static int release_grant(const struct device *dev,
+			 grant_ref_t ref,
+			 grant_ref_t *gref_head,
+			 int flags)
+{
+	if (!gnttab_end_foreign_access_ref(ref, flags)) {
+		dev_alert(dev,
+			  "Grant still in use by backend domain\n");
+		return 1;
+	}
+
+	gnttab_release_grant_reference(gref_head, ref);
+
+	return 0;
+}
 
 static struct sk_buff *xennet_alloc_one_rx_buffer(struct netfront_queue *queue)
 {
@@ -303,7 +334,7 @@ static void xennet_alloc_rx_buffers(struct netfront_queue *queue)
 		struct sk_buff *skb;
 		unsigned short id;
 		grant_ref_t ref;
-		struct page *page;
+		unsigned long gfn;
 		struct xen_netif_rx_request *req;
 
 		skb = xennet_alloc_one_rx_buffer(queue);
@@ -317,17 +348,16 @@ static void xennet_alloc_rx_buffers(struct netfront_queue *queue)
 		BUG_ON(queue->rx_skbs[id]);
 		queue->rx_skbs[id] = skb;
 
-		ref = gnttab_claim_grant_reference(&queue->gref_rx_head);
-		WARN_ON_ONCE(IS_ERR_VALUE((unsigned long)(int)ref));
+		gfn = xen_page_to_gfn(skb_frag_page(&skb_shinfo(skb)->frags[0]));
+		ref = claim_grant(gfn,
+				  &queue->gref_rx_head,
+				  queue->info->xbdev->otherend_id,
+				  0);
+
 		queue->grant_rx[id].ref = ref;
 
-		page = skb_frag_page(&skb_shinfo(skb)->frags[0]);
-
 		req = RING_GET_REQUEST(&queue->rx, req_prod);
-		gnttab_page_grant_foreign_access_ref_one(ref,
-							 queue->info->xbdev->otherend_id,
-							 page,
-							 0);
+
 		req->id = id;
 		req->gref = ref;
 	}
@@ -426,14 +456,13 @@ static bool xennet_tx_buf_gc(struct netfront_queue *queue)
 			queue->tx_link[id] = TX_LINK_NONE;
 			skb = queue->tx_skbs[id];
 			queue->tx_skbs[id] = NULL;
-			if (unlikely(!gnttab_end_foreign_access_ref(
-				queue->grant_tx[id].ref, GNTMAP_readonly))) {
-				dev_alert(dev,
-					  "Grant still in use by backend domain\n");
+
+			if (release_grant(dev, queue->grant_tx[id].ref,
+				      &queue->gref_tx_head,
+				      GNTMAP_readonly)) {
 				goto err;
 			}
-			gnttab_release_grant_reference(
-				&queue->gref_tx_head, queue->grant_tx[id].ref);
+
 			queue->grant_tx[id].ref = GRANT_INVALID_REF;
 			queue->grant_tx[id].page = NULL;
 			add_id_to_list(&queue->tx_skb_freelist, queue->tx_link, id);
@@ -479,11 +508,10 @@ static void xennet_tx_setup_grant(unsigned long gfn, unsigned int offset,
 
 	id = get_id_from_list(&queue->tx_skb_freelist, queue->tx_link);
 	tx = RING_GET_REQUEST(&queue->tx, queue->tx.req_prod_pvt++);
-	ref = gnttab_claim_grant_reference(&queue->gref_tx_head);
-	WARN_ON_ONCE(IS_ERR_VALUE((unsigned long)(int)ref));
-
-	gnttab_grant_foreign_access_ref(ref, queue->info->xbdev->otherend_id,
-					gfn, GNTMAP_readonly);
+	ref = claim_grant(gfn,
+			  &queue->gref_tx_head,
+			  queue->info->xbdev->otherend_id,
+			  GNTMAP_readonly);
 
 	queue->tx_skbs[id] = skb;
 	queue->grant_tx[id].page = page;
@@ -936,15 +964,13 @@ static int xennet_get_responses(struct netfront_queue *queue,
 			goto next;
 		}
 
-		if (!gnttab_end_foreign_access_ref(ref, 0)) {
-			dev_alert(dev,
-				  "Grant still in use by backend domain\n");
+		if (release_grant(dev, ref,
+			      &queue->gref_rx_head,
+			      0)) {
 			queue->info->broken = true;
 			dev_alert(dev, "Disabled for further use\n");
 			return -EINVAL;
 		}
-
-		gnttab_release_grant_reference(&queue->gref_rx_head, ref);
 
 		__skb_queue_tail(list, skb);
 
