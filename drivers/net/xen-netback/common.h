2ba2e913341c3acfa708aee01f3782c71831eb09
Joao Martins <joao.martins@neclab.eu>
2015-05-12 19:18:30 +0200

    xen-netback: copy buffer on xenvif_start_xmit()
    By introducing persistent grants we speed up the RX thread with the
    decreased copy cost, that leads to a throughput decrease of 20%.
    It is observed that the rx_queue stays mostly at 10% of its capacity,
    as opposed to full capacity when using grant copy. And a finer measure
    with lock_stat (below with pkt_size 64, burst 1) shows much higher wait
    queue contention on queue->wq, which hints that the RX kthread is
    waits/wake_up more often, that is actually doing work.
    
    Without persistent grants:
    
    class name    con-bounces    contentions   waittime-min   waittime-max
    waittime-total   waittime-avg    acq-bounces   acquisitions   holdtime-min
    holdtime-max holdtime-total   holdtime-avg
    --------------------------------------------------------------------------
    &queue->wq:   792            792           0.36          24.36
    1140.30           1.44           4208        1002671           0.00
    46.75      538164.02           0.54
    ----------
    &queue->wq    326          [<ffffffff8115949f>] __wake_up+0x2f/0x80
    &queue->wq    410          [<ffffffff811592bf>] finish_wait+0x4f/0xa0
    &queue->wq     56          [<ffffffff811593eb>] prepare_to_wait+0x2b/0xb0
    ----------
    &queue->wq    202          [<ffffffff811593eb>] prepare_to_wait+0x2b/0xb0
    &queue->wq    467          [<ffffffff8115949f>] __wake_up+0x2f/0x80
    &queue->wq    123          [<ffffffff811592bf>] finish_wait+0x4f/0xa0
    
    With persistent grants:
    
    &queue->wq:   61834          61836           0.32          30.12
    99710.27           1.61         241400        1125308           0.00
    75.61     1106578.82           0.98
    ----------
    &queue->wq     5079        [<ffffffff8115949f>] __wake_up+0x2f/0x80
    &queue->wq    56280        [<ffffffff811592bf>] finish_wait+0x4f/0xa0
    &queue->wq      479        [<ffffffff811593eb>] prepare_to_wait+0x2b/0xb0
    ----------
    &queue->wq     1005        [<ffffffff811592bf>] finish_wait+0x4f/0xa0
    &queue->wq    56761        [<ffffffff8115949f>] __wake_up+0x2f/0x80
    &queue->wq     4072        [<ffffffff811593eb>] prepare_to_wait+0x2b/0xb0
    
    Also, with persistent grants, we don't require batching grant copy ops
    (besides the initial copy+map) which makes me believe that deferring
    the skb to the RX kthread just adds up unnecessary overhead (for this
    particular case). This patch proposes copying the buffer on
    xenvif_start_xmit(), which lets us both remove the contention on
    queue->wq and lock on rx_queue. Here, an alternative to
    xenvif_rx_action routine is added namely xenvif_rx_map() that maps
    and copies the buffer to the guest. This is only used when persistent
    grants are used, since it would otherwise mean an hypercall per
    packet.
    
    Improvements are up to a factor of 2.14 with a single queue getting us
    from 1.04 Mpps to 1.7 Mpps (burst 1, pkt_size 64) and 1.5 to 2.6 Mpps
    (burst 2, pkt_size 64) compared to using the kthread. Maximum with grant
    copy is 1.2 Mpps, irrespective of the burst. All of this, measured on
    an Intel(R) Xeon(R) CPU E5-1650 v2 @ 3.50GHz.
    
    Signed-off-by: Joao Martins <joao.martins@neclab.eu>

--- drivers/net/xen-netback/common.h
+++ drivers/net/xen-netback/common.h
@@ -418,6 +418,8 @@ int xenvif_dealloc_kthread(void *data);
 
 irqreturn_t xenvif_ctrl_irq_fn(int irq, void *data);
 
+int xenvif_rx_map(struct xenvif_queue *queue, struct sk_buff *skb);
+
 void xenvif_rx_queue_tail(struct xenvif_queue *queue, struct sk_buff *skb);
 
 void xenvif_carrier_on(struct xenvif *vif);
